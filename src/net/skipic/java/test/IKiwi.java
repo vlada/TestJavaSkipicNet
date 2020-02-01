69bc5cd08dbe14cd96e259e6771f84ca77786bf5
vladimir skipic <skipic@gmail.com>
2020-02-01 18:30:10 +0100

    adding kiwi

---
+++ src/net/skipic/java/test/IKiwi.java
@@ -0,0 +1,5 @@
+package net.skipic.java.test;
+
+public interface IKiwi {
+  void Print();
+}
---
+++ src/net/skipic/java/test/Kiwi.java
@@ -0,0 +1,8 @@
+package net.skipic.java.test;
+
+public class Kiwi implements IKiwi {
+  @Override
+  public void Print() {
+    System.out.println("print from kiwi");
+  }
+}
